d1b61e51e43cc8914c687bee9147e5e7f520ff16
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-06 20:31:52 -0300

    Atualização do curso de Jest com React.

--- sorteador-de-amigo-secreto-main/src/components/Formulario/index.tsx
+++ sorteador-de-amigo-secreto-main/src/components/Formulario/index.tsx
@@ -1,8 +1,33 @@
+import { useRef, useState } from "react";
+import { useAdicionarParticipanteLista } from "../../state/hooks/useAdicionarParticipantesLista";
+import { useMensagemDeErro } from "../../state/hooks/useMensagemDeErro";
+
 export default function Formulario() {
-    return (
-        <form>
-            <input type="text" placeholder="Insira os nomes dos participantes"/>
-            <button disabled >Adicionar</button>
-        </form>
-    )
+  const [nome, setNome] = useState("");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const adicionarNaLista = useAdicionarParticipanteLista();
+  const mensagemErro = useMensagemDeErro()
+
+  const adicionarParticipante = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    adicionarNaLista(nome)
+    inputRef.current?.focus();
+    setNome("")
+  };
+
+  return (
+    <form onSubmit={adicionarParticipante}>
+      <input
+        ref={inputRef}
+        value={nome}
+        type="text"
+        placeholder="Insira os nomes dos participantes"
+        onChange={(event) => setNome(event.target.value)}
+      />
+      <button disabled={!nome}>Adicionar</button>
+      {mensagemErro && <p role="alert">{mensagemErro}</p>}
+    </form>
+  );
 }
