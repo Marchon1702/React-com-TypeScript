af3061bc648224ab50b5687c2da8669de78da5ca
Marchon1702 <matheus.s.marchon@hotmail.com>
2024-11-16 12:59:40 -0300

    Arquivos finais do curso performace de componentes React

--- aluroni-performance-aula1.2/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import Titulo from "./components/Titulo";
-
-export default function App() {
-    console.log(<Titulo />)
-    return (
-        <Titulo />
-    )
-}
--- aluroni-performance-aula1.2/src/components/Titulo/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function Titulo() {
-    return(
-        <h1>Aluroni</h1>
-    )
-} 
